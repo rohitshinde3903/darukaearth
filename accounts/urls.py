57fa2b675e1db06efd114ee3be6283752c5b50e3
rohitshinde3903 <rohitshinde3903@gmail.com>
2025-11-10 17:01:48 +0530

    send all

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api_register/', views.api_register, name='api_register'),
-    path('api_login/', views.api_login, name='api_login'),
+    path('api_register/', views.api_register, name='api_register'),  # POST
+    path('api_login/', views.api_login, name='api_login'),  # POST - not GET
+    path('login/', views.api_login, name='api_login_v2'),  # New endpoint - bypasses cache
 ]
